4b094a409fd7bfd8db82eaf84d02522f41f416e5
ahmadjarrar <>
2021-04-14 12:15:48 +0000

    new new

---
+++ app.js
@@ -0,0 +1,13 @@
+var x = prompt('please enter your age');
+     if(x >= 18){
+  document.write('welcome to page');
+  document.write('<img src="https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._AC_SL1200_.jpg">');
+} 
+else if(x < 18){
+  alert('Very sorry this content may contain violent scenes');
+
+  document.write(' so sory');
+  document.write('<img src="https://i.pinimg.com/736x/b4/42/b3/b442b3c2ac6ac0beffc9e554474a208c.jpg ">');
+  
+}
+  
