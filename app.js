7969093bd3c6000163692b2e0f89bac773e32ed6
ahmadjarrar <>
2021-04-14 12:36:16 +0000

    new

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var x = prompt('please enter your age');
   document.write('<img src="https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._AC_SL1200_.jpg">');
 } 
 else if(x < 18){
-  alert('Very sorry this content may contain violent scenes');
+  alert('Attention It may contain scenes that are not appropriate for your age');
 
   document.write(' so sory');
   document.write('<img src="https://i.pinimg.com/736x/b4/42/b3/b442b3c2ac6ac0beffc9e554474a208c.jpg ">');
