a140f1a16aa6765ce052fc3d2b319299a71ffcec
ahmadjarrar <>
2021-04-15 11:45:39 +0000

    new film

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ else if(x < 18){
   document.write('<img src="https://i.pinimg.com/736x/b4/42/b3/b442b3c2ac6ac0beffc9e554474a208c.jpg ">');
   
 }
-  
+  var y = prompt('Hello, please enter your fav actor name from The Dark Knight movie ?');
+  while(y !== 'jack' && y !== 'bool'){
+  y = prompt('Please enter the name jack or bool');}
+  var z;
+
+if(y === 'jack'){
+z ='<img src="https://www.hiamag.com/sites/default/files/styles/album/public/hiaimages/23/01/2017/49685011580900272.jpg">';
+}
+else if (y === 'bool'){
+  z ='<img src="https://img.youm7.com/xlarge/20190902055206526.jpg">';
+} 
+var imagesNumber= prompt('How many image do you want???');
+
+for(var i=0 ; i < imagesNumber; i++){
+  document.write(z);
+}
